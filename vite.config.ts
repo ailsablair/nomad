46f89acb8ac7bf9b58ac1d19eed4f298c20cea52
Ailsa Blair <109297718+ailsablair@users.noreply.github.com>
2026-07-19 14:59:35 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   base: '/nomad/',
   root: './client',
+  resolve: {
+    alias: {
+      // This tells Vite that @/ refers to the client/src directory
+      '@': path.resolve(__dirname, './client/src'),
+    },
+  },
   build: {
     outDir: '../dist',
     emptyOutDir: true,
